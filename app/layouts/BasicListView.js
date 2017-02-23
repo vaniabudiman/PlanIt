1eb4aaafc1b2b040a9593e019497c8957d46259b
VaniaBudiman <evaniab@msn.com>
2017-02-23 00:19:10 -0800

    Add description view
    - Fix inconsistent indentations
    - Fix Stylesheet imports which weren't actually importing anything (my bad)
    -

--- app/layouts/BasicListView.js
+++ app/layouts/BasicListView.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import ListViewStyles from "../styles/ListViewStyles.js";
+import { ListViewStyles } from "../styles/ListViewStyles.js";
 import Icon from "react-native-vector-icons/FontAwesome";
 import { ListView, Title, Subtitle, Divider, View, Row } from "@shoutem/ui";
 
@@ -9,7 +9,13 @@ export default class BasicListView extends Component {
     constructor (props) {
         super(props);
         this.state = {
-            items: ["Data1", "Data2", "Data3", "Data4", "Data5", "Data6", "Data7", "Data8"]
+            items: [
+            { field: "field name", description: "field description goes here" },
+            { field: "field name2", description: "field description2 goes here" },
+            { field: "field name3", description: "field description3 goes here" },
+            { field: "field name4", description: "field description4 goes here" },
+            { field: "field name5", description: "field description5 goes here" }
+            ]
         };
     }
 
@@ -18,8 +24,8 @@ export default class BasicListView extends Component {
             <Row>
                 <View styleName="horizontal space-between">
                     <View styleName="vertical">
-                        <Title>{item}</Title>
-                        <Subtitle>subtitle to {item}</Subtitle>
+                        <Title>{item.field}</Title>
+                        <Subtitle>{item.description}</Subtitle>
                     </View>
                     <View styleName="horizontal">
                         <Icon styleName="disclosure" name="plus" style={ListViewStyles.optionIcons} size={16} />
--- app/styles/ListViewStyles.js
+++ app/styles/ListViewStyles.js
@@ -1,7 +1,7 @@
 import { StyleSheet } from "react-native";
 
 
-export default StyleSheet.create({
+export const ListViewStyles = StyleSheet.create({
     optionIcons: {
         marginRight: 10
     },
