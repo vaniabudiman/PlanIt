a8dffb170d5a63ce25148ae2350d61238285a69e
Nixon Kwok <nixonkwok@gmail.com>
2017-03-05 00:05:02 -0800

    Add trips, events, and bookmarks routes

--- FlaskSQLAlchemyServer/__init__.py
+++ FlaskSQLAlchemyServer/__init__.py
@@ -171,6 +171,378 @@ def users(userName=None):
     return bad_request()
 
 
+@app.route(VER_PATH + '/trips', methods=[POST, GET], strict_slashes=False)
+@app.route(VER_PATH + '/trips/<int:tripID>', methods=[PUT, DELETE])
+def trips(tripID=None):
+    curr_userName = session.get(KEY__USERNAME)
+    if curr_userName is None:
+        return bad_request()
+
+    if request.method == POST:
+        try:
+            post_tripName = str(request.json['tripID'])
+            post_active = str(request.json['active'])
+            post_startDate = to_datetime(
+                str(request.json.get('startDate', None)))
+            post_endDate = to_datetime(str(request.json.get('active', None)))
+        except (KeyError, ValueError) as err:
+            return bad_request(err)
+
+        db = create_db_session()
+        try:
+            max_id = db.query(
+                func.max(Trip.tripID).label('max_id')).first().max_id
+            if max_id is None:
+                max_id = 0  # No entries created yet.
+            trip = Trip(max_id + 1, post_tripName, post_active,
+                        post_startDate, post_endDate, curr_userName)
+        except ValueError as ve:
+            close_session(db)
+            return bad_request(ve)
+
+        try:
+            db.add(trip)
+            db.commit()
+            return make_response(jsonify({'trip': trip.to_dict()}), 201)
+        except IntegrityError:
+            db.rollback()
+            # This should not occur given that we auto increment max EventID.
+            # This might occur if multiple users are adding at the same time.
+            return make_response('Conflict - TripID taken.', 409)
+        finally:
+            close_session(db)
+    elif request.method == GET:
+        post_tripID = int(request.json.get('tripID', None))
+
+        db = create_db_session()
+        try:
+            if post_tripID:
+                trip = db.query(Trip).filter(Trip.tripID == post_tripID).first()
+                if trip is None:
+                    return make_response('Trip not found.', 404)
+                if trip.userName != curr_userName:
+                    return make_response(
+                        'User not authorized to view this Trip.', 401)
+                return make_response(jsonify({'trip': [trip.to_dict]}), 200)
+            else:
+                trip_list = db.query(Trip).filter(
+                    Trip.userName == curr_userName).all()
+                if len(trips) == 0:
+                    return make_response('No Trips found.', 404)
+                trips_dict = {'trip': [trip.to_dict() for trip in trip_list]}
+                return make_response(jsonify(trips_dict), 200)
+        finally:
+            close_session(db)
+    elif tripID:
+        db = create_db_session()
+        trip = db.query(Trip).filter(Trip.tripID == tripID).first()
+        if trip is None:
+            close_session(db)
+            return make_response('Trip not found.', 404)
+        userName = trip.userName
+
+        if request.method == PUT:
+            if userName != curr_userName:
+                close_session(db)
+                return make_response('User not authorized to edit Trip.', 401)
+            try:
+                # Optional tripName parameter.
+                post_tripName = str(request.json['tripName'])
+                trip.tripName = post_tripName
+            except KeyError:
+                pass
+
+            try:
+                # Optional active parameter.
+                post_active = request.json['active']
+                trip.active = post_active
+            except KeyError:
+                pass
+
+            try:
+                # Optional name startDate.
+                post_startDate = str(request.json['startDate'])
+                trip.startDate = to_datetime(post_startDate)
+            except ValueError as ve:
+                close_session(db)
+                return bad_request(ve)
+            except KeyError:
+                pass
+
+            try:
+                # Optional endDate parameter.
+                post_endDate = str(request.json['endDate'])
+                trip.endDate = to_datetime(post_endDate)
+            except ValueError as ve:
+                close_session(db)
+                return bad_request(ve)
+            except KeyError:
+                pass
+
+            ret_dict = {'trip': trip.to_dict()}
+            close_session(db)
+            return make_response(jsonify(ret_dict), 200)
+        elif request.method == DELETE:
+            if userName != curr_userName:
+                close_session(db)
+                return make_response('User not authorized to delete Trip.', 401)
+            db.delete(trip)
+            close_session(db)
+            return make_response('Event deleted successfully', 200)
+    return bad_request()
+
+
+@app.route(VER_PATH + '/events', methods=[POST, GET], strict_slashes=False)
+@app.route(VER_PATH + '/events/<int:eventID>', methods=[PUT, DELETE])
+def events(eventID=None):
+    if request.method == POST:
+        try:
+            post_tripID = int(request.json['tripID'])
+            post_events = request.json['events']
+        except KeyError:
+            return bad_request()
+
+        db = create_db_session()
+        try:
+            trip = db.query(Trip).filter(Trip.tripID == post_tripID).first()
+            if trip is None:
+                return make_response('Trip not found.', 404)
+            if trip.userName != session.get(KEY__USERNAME):
+                return make_response('User not authorized to add to this Trip.',
+                                     401)
+        finally:
+            close_session(db)
+
+        if len(post_events) == 0:
+            # There should be at least one Event to work with.
+            return bad_request()
+
+        db = create_db_session()
+        try:
+            max_id = db.query(
+                func.max(Event.eventID).label('max_id')).first().max_id
+            if max_id is None:
+                max_id = 0  # No entries created yet.
+            event_list = []
+
+            for event in post_events:
+                max_id += 1
+                event_list.append(Event(max_id,
+                                        event['eventName'],
+                                        to_datetime(event['startDateTime']),
+                                        to_datetime(event['endDateTime']),
+                                        event.get('locationID'),
+                                        None,
+                                        None,
+                                        post_tripID))
+        except (KeyError, ValueError) as err:
+            close_session(db)
+            return bad_request(err)
+
+        try:
+            db.add_all(event_list)
+            db.commit()
+            events_dict = {'event': [event.to_dict() for event in event_list]}
+            return make_response(jsonify(events_dict), 201)
+        except IntegrityError:
+            db.rollback()
+            # This should not occur given that we auto increment max EventID.
+            # This might occur if multiple users are adding at the same time.
+            return make_response('Conflict - EventID taken.', 409)
+        finally:
+            close_session(db)
+    elif request.method == GET:
+        try:
+            post_tripID = int(request.json['tripID'])
+            post_eventID = int(request.json['eventID'])
+        except KeyError:
+            return bad_request()
+
+        db = create_db_session()
+        try:
+            trip = db.query(Trip).filter(Trip.tripID == post_tripID).first()
+            event = db.query(Event).filter(
+                Event.eventID == post_eventID).first()
+            if trip is None:
+                return make_response('Trip not found.', 404)
+            if event is None:
+                return make_response('Event not found.', 404)
+            if trip.userName != session.get(KEY__USERNAME):
+                return make_response('User not authorized to view this Event.',
+                                     401)
+            return make_response(jsonify({'event': event.to_dict}), 200)
+        finally:
+            close_session(db)
+    elif eventID:
+        db = create_db_session()
+        curr_userName = session.get(KEY__USERNAME)
+        event = db.query(Event).filter(Event.eventID == eventID).first()
+        if event is None:
+            close_session(db)
+            return make_response('Event not found.', 404)
+        trip = db.query(Trip).filter(Trip.tripID == event.tripID).first()
+        if trip is None:
+            close_session(db)
+            return make_response('Trip for given Event not found.', 404)
+        userName = trip.userName
+
+        if request.method == PUT:
+            if userName != curr_userName:
+                close_session(db)
+                return make_response('User not authorized to edit Event.',
+                                     401)
+            try:
+                # Optional eventName parameter.
+                post_eventName = str(request.json['eventName'])
+                event.eventName = post_eventName
+            except KeyError:
+                pass
+
+            try:
+                # Optional name startDateTime.
+                post_startDateTime = str(request.json['startDateTime'])
+                event.startDateTime = to_datetime(post_startDateTime)
+            except ValueError as ve:
+                close_session(db)
+                return bad_request(ve)
+            except KeyError:
+                pass
+
+            try:
+                # Optional endDateTime parameter.
+                post_endDateTime = str(request.json['endDateTime'])
+                event.endDateTime = to_datetime(post_endDateTime)
+            except ValueError as ve:
+                close_session(db)
+                return bad_request(ve)
+            except KeyError:
+                pass
+
+            try:
+                # Optional locationID parameter.
+                post_locationID = int(request.json['locationID'])
+                event.locationID = post_locationID
+            except KeyError:
+                pass
+
+            ret_dict = {'event': event.to_dict()}
+            close_session(db)
+            return make_response(jsonify(ret_dict), 200)
+        elif request.method == DELETE:
+            if userName != curr_userName:
+                close_session(db)
+                return make_response('User not authorized to delete Event.',
+                                     401)
+            db.delete(event)
+            close_session(db)
+            return make_response('Event deleted successfully', 200)
+    return bad_request()
+
+
+@app.route(VER_PATH + '/bookmarks', methods=[POST, GET], strict_slashes=False)
+@app.route(VER_PATH + '/bookmarks/<int:bookmarkID>', methods=[PUT, DELETE])
+def bookmarks(bookmarkID=None):
+    if request.method == POST:
+        try:
+            post_tripID = int(request.json['tripID'])
+            post_bookmarks = request.json['events']
+        except KeyError:
+            return bad_request()
+
+        db = create_db_session()
+        try:
+            trip = db.query(Trip).filter(Trip.tripID == post_tripID).first()
+            if trip is None:
+                return make_response('Trip not found.', 404)
+            if trip.userName != session.get(KEY__USERNAME):
+                return make_response(
+                    'User not authorized to add Bookmark to this Trip.', 401)
+        finally:
+            close_session(db)
+
+        if len(post_bookmarks) == 0:
+            # There should be at least one Bookmark to work with.
+            return bad_request()
+
+        db = create_db_session()
+        try:
+            max_id = db.query(
+                func.max(Bookmark.bookmarkID).label('max_id')).first().max_id
+            if max_id is None:
+                max_id = 0  # No entries created yet.
+            bookmark_list = []
+
+            for bookmark in post_bookmarks:
+                max_id += 1
+                bookmark_list.append(
+                    Bookmark(max_id,
+                             bookmark['locationID'],
+                             post_tripID,
+                             bookmark.get('eventID', None)))
+        except KeyError as ke:
+            close_session(db)
+            return bad_request(ke)
+
+        try:
+            db.add_all(bookmark_list)
+            db.commit()
+            bookmarks_dict = {
+                'events': [bookmark.to_dict() for bookmark in bookmark_list]}
+            return make_response(jsonify(bookmarks_dict), 201)
+        except IntegrityError:
+            db.rollback()
+            # This should not occur given that we auto increment max EventID.
+            # This might occur if multiple users are adding at the same time.
+            return make_response('Conflict - BookmarkID taken.', 409)
+        finally:
+            close_session(db)
+    elif request.method == GET:
+        try:
+            post_tripID = int(request.json['tripID'])
+            post_bookmarkID = int(request.json['bookmarkID'])
+        except KeyError:
+            return bad_request()
+
+        db = create_db_session()
+        try:
+            trip = db.query(Trip).filter(Trip.tripID == post_tripID).first()
+            bookmark = db.query(Bookmark).filter(
+                Bookmark.bookmarkID == post_bookmarkID).first()
+            if trip is None:
+                return make_response('Trip not found.', 404)
+            if bookmark is None:
+                return make_response('Bookmark not found.', 404)
+            if trip.userName != session.get(KEY__USERNAME):
+                return make_response(
+                    'User not authorized to view this Bookmark.', 401)
+            return make_response(jsonify({'bookmark': bookmark.to_dict}), 200)
+        finally:
+            close_session(db)
+    elif bookmarkID:
+        db = create_db_session()
+        curr_userName = session.get(KEY__USERNAME)
+        bookmark = db.query(Bookmark).filter(
+            Bookmark.bookmarkID == bookmarkID).first()
+        if bookmark is None:
+            close_session(db)
+            return make_response('Bookmark not found.', 404)
+        trip = db.query(Trip).filter(Trip.tripID == bookmark.tripID).first()
+        if trip is None:
+            close_session(db)
+            return make_response('Trip for given Bookmark not found.', 404)
+        userName = trip.userName
+
+        if request.method == DELETE:
+            if userName != curr_userName:
+                close_session(db)
+                return make_response('User not authorized to delete Bookmark.',
+                                     401)
+            db.delete(bookmark)
+            close_session(db)
+            return make_response('Bookmark deleted successfully', 200)
+    return bad_request()
+
+
 @app.route(VER_PATH + '/login', methods=[POST], strict_slashes=False)
 def login():
     """ /:{version}/login
