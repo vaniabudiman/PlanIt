d0af87b8ff48b4802e70e4ea10c81339ec6c5265
Vania Budiman <evaniab@msn.com>
2017-03-07 19:48:52 -0800

    Add missing import Types

--- app/layouts/UserProfileView.js
+++ app/layouts/UserProfileView.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
 import { GlobalStyles } from "../styles/GlobalStyles.js";
+import FormTemplate, { Types } from "../templates/FormTemplate.js";
 
 
 // TODO: remove these mocks
