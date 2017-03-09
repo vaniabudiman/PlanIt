6226532d9011135d64c1860d957a260cb5ffb6d6
Sisi Guo <sisi.guo.123@gmail.com>
2017-03-09 01:26:31 -0800

    Match continents tile spacing to trips home

--- app/styles/ContinentStyles.js
+++ app/styles/ContinentStyles.js
@@ -2,10 +2,6 @@ import { StyleSheet } from "react-native";
 
 
 export default StyleSheet.create({
-    tileSpacing: {
-        marginRight: 5,
-        marginBottom: 5
-    },
     touchOpacitySizing: {
         // Copied over from @shoutem/ui theme.js for shoutem.ui.Image
         flexDirection: "column",
